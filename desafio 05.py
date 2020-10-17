59b05ad57f419ed352da04e6173dee2f9ff30ca3
danielamorimdesa <dan-amorim@live.com>
2020-10-16 21:25:49 -0300

    Exercícios iniciais

---
+++ desafio 05.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+sucessor = n1 + 1
+antecessor = n1 - 1
+print('Seu sucessor é: {}'.format(sucessor))
+print('Seu antecessor é: {}'.format(antecessor))
---
+++ desafio 06.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número: '))
+dobro = n1 * 2
+triplo = n1 * 3
+raiz_quadrada = n1**(1/2)
+print('Seu dobro: {}'.format(dobro))
+print('Seu triplo: {}'.format(triplo))
+print('Sua raiz quadrada: {:.2f}'.format(raiz_quadrada))
